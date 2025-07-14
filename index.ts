197bdd76aac28a87ba495beb56b4d6f4077bd84e
Uzair Hussain <uzair.hussain@rewardops.com>
2025-07-14 21:42:55 +0500

    refactor: added db connection to the app

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { app } from '~/app';
 import { PORT } from '~/config';
+import { connectDB } from '~/db/connect';
 import { logger } from '~/utils/logger';
 
 function bootstrapApp() {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    logger.info('Trying to establish db connection');
+    await connectDB();
     logger.info(`Server is listening on ${PORT}`);
   });
 }
