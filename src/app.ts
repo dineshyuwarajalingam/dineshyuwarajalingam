6d1d60f378b863d7535268592394b4c3e6cc5dc5
Uzair Hussain <uzair.hussain@rewardops.com>
2025-07-14 21:17:26 +0500

    refactor: updated the app file to only contain app related stuff

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
-// const express = require('express');
-// const marqetaConfig = require('./marqetaConfig');
-// const axios = require('axios');
-// const app = express();
-
 import express from 'express';
 import cors from 'cors';
-import axios from 'axios';
 
-import { marqetaConfig } from './marqetaConfig';
+import api from './api';
+import { NODE_ENV } from './config';
 
 const app = express();
 
@@ -15,67 +10,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.get('/', (req, res) => {
-//   res.send('Server is working!');
-// });
+app.use('/api', api);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
 
-// Register user endpoint with Marqeta API call
-// Function to register user with Marqeta
-async function createUser(member) {
-  if (!member.first_name || !member.last_name || !member.email) {
-    throw new Error('first_name, last_name, and email are required');
-  }
-
-  const { baseUri, username, password } = marqetaConfig;
-
-  const userResponse = await axios.post(`${baseUri}/users`, member, {
-    auth: {
-      username,
-      password,
-    },
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  res.status(err?.status ?? 500).json({
+    message: err.message ?? 'Something went wrong',
+    ...(NODE_ENV === 'development' && { stack: err.stack }),
   });
-
-  const userToken = userResponse.data.token;
-
-  const cardPayload = {
-    card_product_token: 'test_token',
-    user_token: userToken,
-  };
-
-  const cardResponse = await axios.post(`${baseUri}/cards`, cardPayload, {
-    auth: {
-      username,
-      password,
-    },
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  return { user: userResponse.data, card: cardResponse.data };
-}
-
-// Register user endpoint
-app.post('/register-user', async (req, res) => {
-  try {
-    const userData = await createUser(req.body);
-    res.status(201).json({
-      message: 'User registered with Marqeta successfully',
-      data: userData,
-    });
-  } catch (error) {
-    console.error(
-      'Error registering user with Marqeta:',
-      error.response?.data || error.message
-    );
-    res.status(500).json({
-      error: 'Failed to register user with Marqeta',
-      details: error.response?.data,
-    });
-  }
 });
 
 export { app };
